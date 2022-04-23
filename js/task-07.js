afe0365977f3ec564ae31329a46569dfc8f7b800
Sergii Proiava <seregikpro@gmail.com>
2022-04-24 01:02:58 +0300

    fifth commit

--- js/task-07.js
+++ js/task-07.js
@@ -1,2 +1,7 @@
-// const input = document.querySelector('#font-size-control');
-// input.addEventListener('blur', onCheckInputLength);
+const input = document.querySelector('#font-size-control');
+const text = document.querySelector('#text');
+    
+input.addEventListener('change', function(event) {   
+    text.style.fontSize = this.value + "px";
+});
+
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const refs = {
+  buttonEl: document.querySelector('.change-color'),
+  spanColor: document.querySelector('.color'),
+};
+
+refs.buttonEl.addEventListener('click', onChangeColor);
+
+function onChangeColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  refs.spanColor.textContent = getRandomHexColor();
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
